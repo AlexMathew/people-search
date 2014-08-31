22aa268e9945cfa6efbfe821ffb15c0b23c63183
Alex Mathew <alexmathew003@gmail.com>
2014-09-01 01:30:17 +0530

    Remove boilerplate

--- routes.py
+++ routes.py
@@ -15,8 +15,6 @@ def search():
 	query = str(request.form['query'])
 	print query, type(query)
 	data = scrape_page(query)
+	print data
 	return render_template('result.html', data=data)
 
-
-if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=7777, debug=True)
